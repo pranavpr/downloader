e06a612c286cc226037efaa240b3f7357c57fefd
pranavpr <pranav@pranavprakash.net>
2014-09-27 00:59:49 +0530

    Improve test

--- test/test.c
+++ test/test.c
@@ -11,19 +11,18 @@ int main(int argc, char *argv[])
     if (curl)
     {
         curl_easy_setopt(curl, CURLOPT_URL, "https://google.com");
-        /* example.com is redirected, so we tell libcurl to follow redirection */
+        /* google.com is redirected, so we tell libcurl to follow redirection */
         curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
-        /* http://curl.haxx.se/docs/caextract.html */
+        /* Provide CA Certs from http://curl.haxx.se/docs/caextract.html */
         curl_easy_setopt(curl, CURLOPT_CAINFO, "ca-bundle.crt");
 
         /* Perform the request, res will get the return code */
         res = curl_easy_perform(curl);
         /* Check for errors */
         if(res != CURLE_OK)
-            fprintf(stderr, "curl_easy_perform() failed: %s\n",
-                    curl_easy_strerror(res));
+            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
 
-        /* always cleanup */
+        /* Always cleanup */
         curl_easy_cleanup(curl);
     }
 
